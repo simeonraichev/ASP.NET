8012a3c284b90561cc574e3c3aa1adb446fcb789
Simeon Raychev <simeon0022@gmail.com>
2022-09-26 23:44:41 +0300

    Exercises-ASP.NET Core Introduction

---
+++ ASP.NET Fundamentals/Exercises-ASP.NET Core Introduction/Exercises-ASP.NET Core Introduction/Controllers/ProductsController.cs
@@ -0,0 +1,43 @@
+﻿using Exercises_ASP.NET_Core_Introduction.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exercises_ASP.NET_Core_Introduction.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private IEnumerable<ProductViewModel> products = new List<ProductViewModel>()
+        {
+            new ProductViewModel()
+            {
+                Id = 1,
+                Name = "Cheese",
+                Price = 7.00
+            },
+            new ProductViewModel()
+            {
+                Id = 2,
+                Name = "Ham",
+                Price = 5.50
+            },
+            new ProductViewModel()
+            {
+                Id = 3,
+                Name = "Bread",
+                Price = 1.50
+            }
+        };
+        public IActionResult ById(int id)
+        {
+            var product = this.products.FirstOrDefault(p=> p.Id == id);
+            if (product == null)
+            {
+                return BadRequest();
+            }
+            return View();
+        }
+        public IActionResult All()
+        {
+            return View();
+        }
+    }
+}
---
+++ ASP.NET Fundamentals/Exercises-ASP.NET Core Introduction/Exercises-ASP.NET Core Introduction/Models/ProductViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Exercises_ASP.NET_Core_Introduction.Models
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+    }
+}
