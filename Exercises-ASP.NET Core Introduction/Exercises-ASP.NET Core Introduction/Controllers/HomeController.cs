fef4c1a9a1da3d3303967def9b43c6a2e9b2876a
Simeon Raychev <simeon0022@gmail.com>
2022-09-26 10:00:06 +0300

    Exercises: ASP.NET Core Introduction

---
+++ Exercises-ASP.NET Core Introduction/Exercises-ASP.NET Core Introduction/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Exercises_ASP.NET_Core_Introduction.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Exercises_ASP.NET_Core_Introduction.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Hello world!";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            ViewBag.Message = "This is an .NET Core MVC Web app!";
+            return View();
+        }
+        public IActionResult Numbers(int count = 3)
+        {
+            ViewBag.Count = count;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
