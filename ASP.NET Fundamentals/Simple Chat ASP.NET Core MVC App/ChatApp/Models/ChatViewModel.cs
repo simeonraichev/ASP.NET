1c7d570b86fdc394df2e0eef3251a329b25dee6a
Simeon Raychev <simeon0022@gmail.com>
2022-09-27 23:39:46 +0300

    Simple Chat ASP.NET Core MVC App

---
+++ ASP.NET Fundamentals/Simple Chat ASP.NET Core MVC App/ChatApp/Models/ChatViewModel.cs
@@ -0,0 +1,12 @@
+﻿using ChatApp.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApp.Models
+{
+    public class ChatViewModel
+    {
+        public MessageViewModel CurrentMessage { get; set; }
+
+        public List<MessageViewModel> Messages { get; set; }
+    }
+}
---
+++ ASP.NET Fundamentals/Simple Chat ASP.NET Core MVC App/ChatApp/Models/MessageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using ChatApp.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApp.Models
+{
+    public class MessageViewModel
+    {
+        public string Sender { get; set; }
+
+        public string MessageText { get; set; }
+        
+    }
+}
