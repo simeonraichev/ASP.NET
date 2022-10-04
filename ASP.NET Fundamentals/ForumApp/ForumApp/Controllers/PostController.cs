349d984e3f13c3bed898e15e5e571437e8359226
Simeon Raychev <simeon0022@gmail.com>
2022-10-04 21:22:00 +0300

    Adding the PostViewModel

---
+++ ASP.NET Fundamentals/ForumApp/ForumApp/Controllers/PostController.cs
@@ -0,0 +1,31 @@
+﻿using ForumApp.Data;
+using ForumApp.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ForumApp.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly ForumDbContext context;
+
+        public PostController(ForumDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = await context.Posts
+                .Select(p=> new PostViewModel()
+                {
+                    Id = p.Id,
+                    Title = p.Title,
+                    Content = p.Content
+                }).ToListAsync();
+
+
+            return View(model);
+        }
+    }
+}
